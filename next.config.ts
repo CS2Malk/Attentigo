84b7938a6d8704ab01bf7672faf266c85fb11eb7
Malcolm Celestin <malcolm.celestin@gmail.com>
2025-07-01 10:51:52 -0400

    vercel deployment fix attempt 3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // next.config.js
+  experimental: {
+    optimizeCss: false, // disables lightningcss
+  },
 };
 
 export default nextConfig;
